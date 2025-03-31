031eaed8e35b71f07297f39818a0c4d7868200b6
kássia kellem <kassiakellem4@gmail.com>
2025-03-31 20:04:44 -0300

    Fix: Correção na funcionalidade do botão 'Cursos e Eventos'

--- www/js/evento.js
+++ www/js/evento.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const cursosContainer = document.getElementById("produtos");
 
     function filtrarCategoria(category) {
+        console.log("category",category)
         if (category === "cursos") {
             cursosContainer.style.display = "flex";  // Mostra os cursos
             eventosContainer.style.display = "none"; // Esconde os eventos
@@ -13,17 +14,19 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         // Atualiza o botão ativo
-        buttons.forEach(btn => btn.classList.remove("active"));
-        const activeButton = document.querySelector(`[data-category="${category}"]`);
-        if (activeButton) {
-            activeButton.classList.add("active");
-        }
+        buttons.forEach(btn => {
+            btn.classList.remove("active");
+            if (btn.getAttribute("data-category") === category) {
+                btn.classList.add("active");
+            }
+        });
     }
 
     // Adiciona evento de clique nos botões
     buttons.forEach(button => {
         button.addEventListener("click", function () {
             const category = this.getAttribute("data-category");
+           console.log("botao clicado", category);
             filtrarCategoria(category);
         });
     });
